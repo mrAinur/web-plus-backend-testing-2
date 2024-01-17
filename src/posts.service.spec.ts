1713fc4fe6228ca65c7bc929598ce69a0f990c73
Ainur <mr.ainur.02@mail.ru>
2024-01-17 16:13:14 +0500

    версия 1

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,32 +1,152 @@
-import { PostsService } from './posts.service';
+import { PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
 
   beforeEach(() => {
     postsService = new PostsService();
   });
 
-  describe('.findMany', () => {
+  describe(".findMany", () => {
     const posts = [
-      {text: 'Post 1'},
-      {text: 'Post 2'},
-      {text: 'Post 3'},
-      {text: 'Post 4'},
+      { text: "Post 1" },
+      { text: "Post 2" },
+      { text: "Post 3" },
+      { text: "Post 4" }
     ];
 
     beforeEach(() => {
-      posts.forEach((post) => postsService.create(post));
+      posts.forEach(post => postsService.create(post));
     });
 
-    it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+    it("should add a new post", () => {
+      const testPost = postsService.create({ text: "Post 5" });
+      expect(testPost).toEqual(
+        expect.objectContaining({
+          id: "5",
+          text: "Post 5"
+        })
+      );
     });
 
-    it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+    it("should find a post", () => {
+      const idPost = "5";
+      const postText = { text: "Post 5" };
+      postsService.create(postText);
+      const testFindPost = postsService.find(idPost);
+      expect(testFindPost).toEqual(
+        expect.objectContaining({
+          id: idPost,
+          text: postText.text
+        })
+      );
     });
 
-    // реализуйте недостающие тест-кейсы
+    it("should return all posts if called without options", () => {
+      const postsTest = postsService.findMany();
+      expect(postsTest).toEqual(
+        expect.arrayContaining([
+          {
+            id: "1",
+            text: "Post 1"
+          },
+          {
+            id: "2",
+            text: "Post 2"
+          },
+          {
+            id: "3",
+            text: "Post 3"
+          },
+          {
+            id: "4",
+            text: "Post 4"
+          }
+        ])
+      );
+    });
+
+    it("should return correct posts for skip options", () => {
+      const skipTest = 2;
+      const testPosts = postsService.findMany({ skip: skipTest });
+      expect(testPosts).toEqual(
+        expect.arrayContaining([
+          {
+            id: "3",
+            text: "Post 3"
+          },
+          {
+            id: "4",
+            text: "Post 4"
+          }
+        ])
+      );
+    });
+
+    it("should return correct posts for limit options", () => {
+      const limitTest = 2;
+      const testPosts = postsService.findMany({ limit: limitTest });
+      expect(testPosts).toEqual(
+        expect.arrayContaining([
+          {
+            id: "1",
+            text: "Post 1"
+          },
+          {
+            id: "2",
+            text: "Post 2"
+          }
+        ])
+      );
+    });
+
+    it("should return correct posts after delete", () => {
+      const deleteId = "2";
+      postsService.delete(deleteId);
+      const postsTest = postsService.findMany();
+      expect(postsTest).toEqual(
+        expect.arrayContaining([
+          {
+            id: "1",
+            text: "Post 1"
+          },
+          {
+            id: "3",
+            text: "Post 3"
+          },
+          {
+            id: "4",
+            text: "Post 4"
+          }
+        ])
+      );
+    });
+
+    it("should update post", () => {
+      const postId = "4";
+      const updateText = { id: postId, text: "Post 4 update" };
+      postsService.update(postId, updateText);
+      const postsTest = postsService.findMany();
+      expect(postsTest).toEqual(
+        expect.arrayContaining([
+          {
+            id: "1",
+            text: "Post 1"
+          },
+          {
+            id: "2",
+            text: "Post 2"
+          },
+          {
+            id: "3",
+            text: "Post 3"
+          },
+          {
+            id: "4",
+            text: updateText.text
+          }
+        ])
+      );
+    });
   });
 });
