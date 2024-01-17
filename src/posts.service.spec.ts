91e15c8ed1f399a41abc587227c86f4216692241
Ainur <mr.ainur.02@mail.ru>
2024-01-17 17:23:02 +0500

    версия 9

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -43,22 +43,35 @@ describe("PostsService", () => {
       );
     });
 
-    it("should return correct posts for skip and limit options", () => {
-      const skipTest = 1;
-      const limitTest = 2;
-      const testPosts = postsService.findMany({
-        skip: skipTest,
-        limit: limitTest
-      });
+    it("should return correct posts for skip options", () => {
+      const skipTest = 2;
+      const testPosts = postsService.findMany({ skip: skipTest });
       expect(testPosts).toEqual(
         expect.arrayContaining([
           {
-            id: "2",
-            text: "Post 2"
-          },
-          {
             id: "3",
             text: "Post 3"
+          },
+          {
+            id: "4",
+            text: "Post 4"
+          }
+        ])
+      );
+    });
+
+    it("should return correct posts for limit options", () => {
+      const limitTest = 2;
+      const testPosts = postsService.findMany({ limit: limitTest });
+      expect(testPosts).toEqual(
+        expect.arrayContaining([
+          {
+            id: "1",
+            text: "Post 1"
+          },
+          {
+            id: "2",
+            text: "Post 2"
           }
         ])
       );
